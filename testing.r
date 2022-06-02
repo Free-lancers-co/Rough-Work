3a64e7722cee1edb105feb9d7f965c0b0e9425e2
oladejo abdullahi <67918882+maxwizardth@users.noreply.github.com>
2022-06-02 23:19:17 +0100

    Add files via upload

---
+++ testing.r
@@ -0,0 +1,14 @@
+
+
+### Create Data Fram
+
+df1 = data.frame( Name = c('George','Andrea', 'Micheal','Maggie','Ravi','Xien','Jalpa'),Mathematics1_score=c(62,47,55,74,32,77,86),Mathematics2_score=c(45,78,44,89,66,49,72),Science_score=c(56,52,45,88,33,90,47))
+df1
+
+## row wise maximum using rowMaxs()
+
+
+
+df2=df1[-length(df1$Name),]
+df2
+
