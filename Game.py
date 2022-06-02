e21f07e243f0a21ecfe8346195df54d9b84ff81b
Charlessavic <106740287+Charlessavic@users.noreply.github.com>
2022-06-02 23:29:38 +0100

    Create Game.py

---
+++ Game.py
@@ -0,0 +1,18 @@
+loop = 1
+While loop <10:
+noun = input("choose a noun: ")
+p_noun = input("choose a plural noun: ")
+noun2 = input("choose a noun: ")
+Place = input ("Name a place: ")
+adjective = input ("choose an adjective (describing word): ")
+noun3 = input ("choose a noun: ")
+print("Be kind to your", noun," - footed", p_noun)
+print("for a duck may be somebody's", noun2,".")
+print("be kind to your", p_noun, "in", place)
+print("where the weather is always", adjective, ".")
+print("you may think that is this the", noun3,".")
+print("well it is.")
+print(".........")
+break
+"loop = 1"
+loop = loop + 1
